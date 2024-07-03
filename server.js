71fcdd2900cb641c64d954e24097cb6d0cc53385
sarthak-star <sarthakbathla17@gmail.com>
2024-07-03 15:55:40 +0530

    more changes

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const app = express();
 const http = require('http');
 const {Server} = require('socket.io');
 const ACTIONS = require('./src/actions');
+const path = require('path');
 
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static('build'));
+app.use((req,res,next) =>{
+    res.sendFile(path.join(__dirname,'build','index.html'));
+})
+
 
 const userSocketMap = {};
 
